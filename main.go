db604616d361f7509ac8b77e52554c216d04eff7
tikipiya <tikipiya0531@icloud.com>
2026-05-02 23:44:42 +0900

    sec

--- main.go
+++ main.go
@@ -9,3 +9,20 @@ import (
 	"gitlab.com/gomidi/midi/v2"
 	"gitlab.com/gomidi/midi/v2/smf"
 )
+
+func main() {
+	filePath := "input.png"
+	imgFile, err := os.Open(filePath)
+	if err != nil {
+		fmt.Printf("画像が開けない : %v\n", err)
+		return
+	}
+	defer imgFile.Close()
+
+	img, _, err := image.Decode(imgFile)
+	if err != nil {
+		fmt.Printf("画像のでコードに失敗 : %v\n", err)
+		return
+	}
+
+}
