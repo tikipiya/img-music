a87c8706ff9d0fb8aca354ed216088624ac0a4ad
tikipiya <tikipiya0531@icloud.com>
2026-05-02 23:39:42 +0900

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module img-music
+
+go 1.24.5
+
+require gitlab.com/gomidi/midi/v2 v2.3.23 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+import (
+	"fmt"
+	"image"
+	_ "image/jpeg"
+	_ "image/png"
+	"math"
+	"os"
+
+	"gitlab.com/gomidi/midi/v2"
+	"gitlab.com/gomidi/midi/v2/smf"
+)
