eaaf4a980d5279b3962d17515824e9864f24d7a6
tikipiya <tikipiya0531@icloud.com>
2026-05-03 00:02:30 +0900

    third

--- main.go
+++ main.go
@@ -24,5 +24,21 @@ func main() {
 		fmt.Printf("画像のでコードに失敗 : %v\n", err)
 		return
 	}
+	bounds := img.Bounds()
+	width, height := bounds.Max.X, bounds.Max.Y
+	clock := smf.MetricTicks(96)
+	s := smf.New()
+	s.TimeFormat = clock
+	var tr smf.Track
+	tr.Add(0, smf.MetaInstrument("Sonification Piano"))
+	stepX := width / 50
+	if stepX == 0 {
+		stepX = 1
+	}
+	for x := 0; x < width; x += stepX {
+		var totalLuminance float64
+		var totalSaturation float64
+		count := 0
 
+	}
 }
